914301abfc831d08c59ab638f5eca924f8aaf321
Nadia Burnasheva <nadezhda.burnasheva@gmail.com>
2017-06-02 14:29:44 +0200

    initial commit

---
+++ CoreProject/TestClass.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace CoreProject
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void TestTrue()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
---
+++ MixedSolution/TestClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace MixedSolution
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void testTrue()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
